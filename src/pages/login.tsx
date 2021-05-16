8c19ae853548a22d6e8c583cfca9c08ae0703080
emre <emrebadem_@hotmail.com>
2021-05-16 04:15:51 +0300

    Wrote i18n.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,33 +1,38 @@
 import React, { FormEvent, useState } from 'react';
-import TextForm from '../components/TextForm';
 import Button from '../components/Button';
+import TextForm from '../components/TextForm';
+import { t } from '../i18n';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <div
-      style={{
-        flexDirection: 'column',
-        flex: 1,
-        display: 'flex',
-        alignContent: 'center',
-      }}
-    >
-      <TextForm
-        value={username}
-        style={{ height: 10 }}
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
-      <TextForm
-        value={password}
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <Button>ssadf</Button>
+    <div className="loginContainer">
+      <div className="loginRoot">
+        <div className="loginForm">
+          <TextForm
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            placeHolder={t('username')}
+          />
+          <TextForm
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeHolder={t('password')}
+          />
+        </div>
+        <div className="loginButton">
+          <Button
+            onClick={() => {
+              console.log('basıldı');
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
