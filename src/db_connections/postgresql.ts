64ccf5ba8fab4bbe504e550a78cd31c18b00ba32
emre <emrebadem_@hotmail.com>
2021-05-28 02:04:47 +0300

    Postgresql class development.

--- src/db_connections/postgresql.ts
+++ src/db_connections/postgresql.ts
@@ -1,27 +1,73 @@
-import { Client } from 'pg';
+import Pool from 'pg-pool';
+import { Client, PoolClient } from 'pg';
 
-const initDb = async () => {
-  const client = new Client();
-  await client.connect();
-  return client;
-};
-const fetchCollections = (client: Client) => {
-  client.query(
-    "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
-  );
-  return;
-};
-const fetchAllFieldsLazily = (client: Client) => {
-  client.query('');
-  return;
-};
-const fetchFieldsLazily = (client: Client) => {
-  client.query('');
+class postgres {
+  currentTable = '';
 
-  return;
-};
-const editField = (client: Client) => {
-  client.query('');
+  user: string;
 
-  return;
-};
+  password: string;
+
+  host: string;
+
+  port: number;
+
+  private fieldsToFetch = '*';
+
+  client: (Client & PoolClient) | undefined;
+
+  // get fieldsToFetch() {
+  //   return this._fieldsToFetch
+  // }
+
+  constructor(user: string, password: string, host: string, port: number) {
+    this.user = user;
+    this.password = password;
+    this.host = host;
+    this.port = port;
+  }
+
+  initDb = () => {
+    const { host, user, password, port } = this;
+    const client = await new Pool({
+      host,
+      user,
+      password,
+      port,
+    }).connect();
+    this.client = client;
+  };
+
+  selectTable = async (tableName: string) => {
+    this.currentTable = tableName;
+  };
+
+  fetchTables = async (client: Client) => {
+    const result = await client.query(
+      "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+    );
+    return result;
+  };
+
+  fetchAllFieldsLazily = (client: Client) => {
+    client.query('UPDATE ${this.currentTable} set $ ');
+    return;
+  };
+
+  *fetchFieldsLazily(client: Client,query: Query fieldsToFetch?: string[]) {
+    const joinedFieldsToFetch = fieldsToFetch ? fieldsToFetch.join() : '*';
+    client.query(``);
+    return;
+  }
+
+  editField = (
+    client: Client,
+    fieldName: string,
+    newValue: string,
+    fieldId: string
+  ) => {
+    client.query(
+      `update dummy_table set ${fieldName}=${newValue} where id=${fieldId}`
+    );
+  };
+}
